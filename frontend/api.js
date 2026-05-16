d33fc7885d819977aa71b967b891a33c24c8979c
Praveen08-pravi <praveenupraveenu98@gmail.com>
2026-05-16 20:13:37 +0530

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -4,7 +4,7 @@ import { isTokenExpired } from "./src/utils/auth";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://snapcart.onrender.com/api",
+    "https://snapcart-esrd.onrender.com/api",
 });
 
 API.interceptors.request.use(
